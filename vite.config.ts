de8f7b45f9aafeb911f368b2b09ae0a2174b5d65
arabian263 <arabianking263@gmail.com>
2025-11-01 10:06:19 +0530

    Setup GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/pharmapay-ledger-65200/',
   server: {
     host: "::",
     port: 8080,
